c20d5b4bf4d19cd2a01d46454edee009b1f94c9a
mamun1646 <mimbd01130004@gmail.com>
2022-09-12 16:11:19 +0600

    Crud Operation Nest Rest Api MySQL

---
+++ src/app.module.ts
@@ -0,0 +1,28 @@
+import { Module } from '@nestjs/common';
+import { TypeOrmModule } from '@nestjs/typeorm';
+// import { AppController } from './app.controller';
+
+import { User } from './user/entity/user.entity';
+import { UserModule } from './user/user.module';
+import { ProfileModule } from './profile/profile.module';
+
+@Module({
+  // controllers: [AppController],
+  imports: [
+    UserModule,
+    TypeOrmModule.forRoot({
+      type: 'mysql',
+      host:'localhost',
+      // host: '127.0.0.1',
+      port: 3306,
+      username: 'root',
+      // password: null,
+      password :'',
+      database: 'mamun',
+      entities: [User],
+      synchronize: true,
+    }),
+   
+  ],
+})
+export class AppModule {}
